46b333d0b81900c4200a68e6e51969ce699fb8ef
liehtman <sanekkonst@gmail.com>
2017-05-28 16:06:31 +0300

    commit

---
+++ config.py
@@ -0,0 +1,1 @@
+token = '388189206:AAF04nlbXm_cHj5qi7Lp2sGK2mtG2Aa68Y0'
---
+++ main.py
@@ -0,0 +1,15 @@
+import config
+import telebot
+import os
+from flask import Flask
+
+bot = telebot.TeleBot(config.token)
+server = Flask(__name__)
+
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+	bot.polling(none_stop=True)

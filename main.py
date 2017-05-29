979e2749c43b1930169c731b2ba15ccb9e16acea
liehtman <sanekkonst@gmail.com>
2017-05-30 01:47:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from flask import Flask, request
 bot = telebot.TeleBot(config.token)
 server = Flask(__name__)
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
-
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 @server.route("/bot", methods=['POST'])
 def getMessage():
@@ -23,8 +25,5 @@ def webhook():
     bot.set_webhook(url="https://mgppu.herokuapp.com/")
     return "!", 200
 
-server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-server = Flask(__name__)
-
-# if __name__ == '__main__':
-# 	bot.polling(none_stop=True)
+if __name__ == '__main__':
+	server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))

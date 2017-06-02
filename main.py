d96c127c1230cb98a2a7b013f029e6caa92bff53
liehtman <sanekkonst@gmail.com>
2017-06-02 12:52:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -9,48 +9,57 @@ from telebot import types
 
 bot = telebot.TeleBot(config.token)
 server = Flask(__name__)
-
 scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(config.json_keyfile, scope)
+
 gc = gspread.authorize(credentials)
 table = gc.open("Расписание").sheet1
 session = gc.open("Сессия")
-ses_queries = ['Зачеты', 'Экзамены', 'Консультации']
-logs = open('logs.txt', 'a')
+logs = gc.open("Логи МГППУ").sheet1
 
 @bot.message_handler(commands=['cleanlogs'])
-def get_id(message):
-	bot.send_message(message.chat.id, str(message.chat.id))
+def cleanlogs(message):
+	clean_logs()
+	bot.send_message(message.chat.id, 'Логи очищены')
+
+@bot.message_handler(commands=['showlogs'])
+def showlogs(message):
+	if message.chat.id == config.creator_id:
+		msg = get_logs()
+		bot.send_message(message.chat.id, msg)
+	else: bot.send_message(message.chat.id, 'Эта функция не для Вас, уважаемый:)')
 
 @bot.message_handler(commands=['getid'])
 def get_id(message):
 	bot.send_message(message.chat.id, str(message.chat.id))
 
-@bot.message_handler(commands=['showlogs'])
-def showlogs(message):
-	if message.chat.id == config.creator_id:
-		bot.send_message(message.chat.id, logs.readlines())
-	else: bot.send_message(message.chat.id, 'Эта функция не для вас, уважаемый:)')
-
 @bot.message_handler(commands=['start'])
 def start(message):
 	markup = types.ReplyKeyboardMarkup()
-	markup.add('Расписание на завтра')
-	markup.add('Зачеты')
-	markup.add('Экзамены')
-	markup.add('Консультации')
-	bot.send_message(message.chat.id, "Привет. Я пока мало чего умею", reply_markup = markup)
+	for course in config.courses:
+		markup.add(course)
+	bot.send_message(message.chat.id, "Привет. Выбери свой курс", reply_markup = markup)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
+def reply_message(message):
 	if message.text=='Расписание на завтра':
 		bot.send_message(message.chat.id, 'Подожди, смотрю...')
 		answer = parse_tomorrow()
 		bot.send_message(message.chat.id, answer)
-	elif message.text in ses_queries:
+	elif message.text in config.ses_queries:
 		bot.send_message(message.chat.id, 'Подожди, смотрю...')
 		answer = parse_session(message.text)
 		bot.send_message(message.chat.id, answer)
+	elif message.text in config.courses:        
+		markup = types.ReplyKeyboardMarkup()
+		for spec in config.specializations:
+			markup.add(spec)
+		bot.send_message(message.chat.id, 'Выбери направление', reply_markup = markup)
+	elif message.text in config.specializations:
+		markup = types.ReplyKeyboardMarkup()
+		for query in config.main_queries:
+			markup.add(query)
+		bot.send_message(message.chat.id, 'Что хочешь знать?', reply_markup = markup)
 	else: bot.send_message(message.chat.id, 'Юзай кнопки!')
 	if message.chat.id != config.creator_id:
 		track(message)
@@ -67,21 +76,25 @@ def webhook():
 	return "!", 200		
 
 def parse_tomorrow():
-	msg = 'Итак, завтра у тебя:\n'
 	day = datetime.now().isoweekday() + 1
+	tomorrow = None
+	if day == 5: tomorrow = table.find('EOW')
+	elif day in [6, 7]:
+		return 'Пляши, завтра выходной!'
+	else: tomorrow = table.find(str(day + 1))
 	cell_num = table.find(str(day))
 	r = cell_num.row
 	c = cell_num.col
-	tomorrow = table.find(str(day + 1))
 	tr = tomorrow.row
 	tc = tomorrow.col
 	values_list = []
+	msg = 'Итак, завтра у тебя:\n'
 	for i in range(int(r), int(tr)):
 		t = table.range('B'+str(i)+':F'+str(i))
 		values_list.append([elem.value for elem in t])
 	for i in range(len(values_list)):
 		msg += '{0}){1} у {2}а в аудитории {3} в {4} ({5}) \n'.format(i+1, values_list[i][0], values_list[i][1], values_list[i][2], values_list[i][4], values_list[i][3])
-	return msg
+	return msg	
 
 def parse_session(query):
 	msg = ''
@@ -119,8 +132,47 @@ def parse_session(query):
 
 def track(message):
 	time = datetime.now().replace(microsecond=0)
-	logs.write('{0} {1}: {2} ({3}) \n'.format(message.chat.first_name, message.chat.last_name, message.text, time))
+	i = 1
+	row = True
+	while row:
+		i += 1
+		row = [val for val in logs.row_values(i) if val]
+
+	vals = [message.chat.first_name, message.chat.last_name, message.text, time]
+	cell_list = logs.range('A{0}:D{0}'.format(i))
+	
+	for i in range(len(cell_list)):
+		cell_list[i].value = vals[i]
+	logs.update_cells(cell_list)
 	bot.send_message(config.creator_id, 'Новые логи!')
+
+def get_logs():
+	msg = ''
+	i = 1
+	rows = []
+	row = True
+	while row:
+		i += 1
+		row = [val for val in logs.row_values(i) if val]		
+		rows.append(row)
+
+	rows = rows[:-1]
+	for i, row in enumerate(rows):
+		string = '{0} {1}: {2} ({3}) \n'.format(row[0], row[1], row[2], row[3])
+		msg += string
+	return msg
+
+def clean_logs():
+	i = 1
+	row = True
+	while row:
+		i += 1
+		row = [val for val in logs.row_values(i) if val]
+
+	cell_list = logs.range('A2:D{0}'.format(i))
+	for cell in cell_list:
+		cell.value = ''
+	logs.update_cells(cell_list)
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 server = Flask(__name__)
